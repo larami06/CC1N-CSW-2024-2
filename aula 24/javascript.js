13990e4abd0dcb1c554710fba45cf658eff4dfbc
Larami <laramf@outlook.com>
2024-11-11 22:40:10 -0300

    Aula 24 INTRODUÇÃO A JAVA

---
+++ aula 24/javascript.js
@@ -0,0 +1,13 @@
+document.confirm("você está gostando de JavaScript?")
+
+window.prompt("Está gostando na JS?")
+
+var resposta = window.confirm("Você esta gostando de JavaScript")
+window.alert("O botão selecionado foi" * resposta)
+
+document.writeln("Olá, meu código JavaScript! Usando o Document Writeln pela segunda vez ")
+
+
+document.getElementById('resposta').innerHTML='Resposta de saída: Olá Mundo'
+
+document.writeln("Olá Mundo Javascript");
